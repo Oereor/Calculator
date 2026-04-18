f5be3aa4223cbbc569db821012b8c720b9789640
oereor <unkn0wn___us3r@outlook.com>
2026-04-18 22:11:06 +0800

    Add FrontendViewModel and bind UI to it
    Add a FrontendViewModel implementing INotifyPropertyChanged and a simple RelayCommand to handle number input. Wire the MainWindow DataContext to the new view model, bind UpperText/LowerText to the top/bottom TextBoxes, and introduce a NumberButton style that routes numeric and symbol buttons to NumberCommand (CommandParameter set to the button Content). Rename a few controls (InputTextBox/OutputTextBox → UpperTextBox/LowerTextBox) and add a using for ComponentModel in the code-behind.

---
+++ Calculator/FrontendViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+
+namespace Calculator
+{
+    public class RelayCommand(Action<object> execute, Predicate<object>? canExecute = null) : ICommand
+    {
+        private readonly Action<object> _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        private readonly Predicate<object> _canExecute = canExecute ?? (_ => true);
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute(parameter!);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter!);
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+
+    public class FrontendViewModel : INotifyPropertyChanged
+    {
+        public string UpperText
+        {
+            get => field;
+            set
+            {
+                if (field != value)
+                {
+                    field = value;
+                    OnPropertyChanged(nameof(UpperText));
+                }
+            }
+        }
+
+        public string LowerText
+        {
+            get => field;
+            set
+            {
+                if (field != value)
+                {
+                    field = value;
+                    OnPropertyChanged(nameof(LowerText));
+                }
+            }
+        }
+
+        public ICommand NumberCommand { get; }
+
+        public FrontendViewModel()
+        {
+            UpperText = string.Empty;
+            LowerText = string.Empty;
+            NumberCommand = new RelayCommand(NumberButtonClicked, CanClickNumberButton);
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null!)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void NumberButtonClicked(object parameter)
+        {
+            if (parameter is string number)
+            {
+                LowerText += number;
+            }
+        }
+
+        private bool CanClickNumberButton(object parameter)
+        {
+            return true;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
